10a0807ab5532a0ad3e3ff12871fb4b4a23a5f40
curowl <nel2592@gmail.com>
2023-12-23 14:05:40 -0600

    snappy on chmod

--- config/snappy.php
+++ config/snappy.php
@@ -37,7 +37,8 @@ return [
         'enabled' => true,
         //'binary'  => env('WKHTML_PDF_BINARY', base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),),
         //'binary'  => env('WKHTML_PDF_BINARY', 'C:\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'),
-        'binary'  => base_path('app/bin/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        //'binary'  => base_path('app/bin/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        'binary' => chmod(base_path('app/bin/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'), 0755),
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,
